d6fb3814a6388527d86610d8f8e72d1f679130f8
Cameron <camparker290599@gmail.com>
2022-03-14 10:19:38 +0000

    Create database and tables

--- forms.py
+++ forms.py
@@ -6,10 +6,12 @@ from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 
+# WTForm linked to Bug database
 class AddBugForm(FlaskForm):
     pass
 
 
+# WTForm linked to User database
 class RegisterForm(FlaskForm):
     name = StringField("Full Name", validators=[DataRequired()], render_kw={"placeholder": "Full Name", "class": "form-control"})
     email = StringField("Email Address", validators=[DataRequired(), Email()], render_kw={"placeholder": "Email Address", "class": "form-control"})
@@ -17,12 +19,14 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register", render_kw={"class": "btn form-control btn-primary rounded submit px-3"})
 
 
+# WTForm linked to User database
 class LoginForm(FlaskForm):
     email = StringField("Email Address", validators=[DataRequired(), Email()], render_kw={"placeholder": "Email Address", "class": "form-control"})
     password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Password", "class": "form-control"})
     submit = SubmitField("Login", render_kw={"class": "btn form-control btn-primary rounded submit px-3"})
 
 
+# WTForm linked to Comments database
 class CommentForm(FlaskForm):
     comment = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from forms import AddBugForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
-# from database import Users, Bugs
 import os
 
 # Initialising app
@@ -35,7 +34,53 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bugtracker.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-# Connect to tables
+# Create database tables
+
+
+class Users(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    role = Column(String(250), nullable=True)
+
+    # relationship with Bugs table
+    bugs = relationship("Bugs", back_populates="submitter")
+
+    # relationship with Comment table
+    comments = relationship("Comment", back_populates="commenter")
+
+
+class Bugs(db.Model):
+    __tablename__ = "bugs"
+    id = Column(Integer, primary_key=True)
+    bug_name = Column(String(250), nullable=False)
+    brief_desc = Column(String(250), nullable=False)
+    full_desc = Column(Text(1000), nullable=False)
+    time = Column(Integer, nullable=True)
+    severity = Column(Integer, nullable=True)
+    priority = Column(Integer, nullable=False)
+    date = Column(String(250), nullable=False)
+    img = Column(String, nullable=True)
+
+    # relationship with Users table
+    submitter_id = Column(Integer, ForeignKey("users.id"))
+    submitter = relationship("Users", back_populates="bugs")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+
+    # relationship with Users table
+    commenter_id = Column(Integer, ForeignKey("users.id"))
+    commenter = relationship("Users", back_populates="comments")
+
+
+# Create tables in db
+# db.create_all()
 
 
 # user_loader callback
